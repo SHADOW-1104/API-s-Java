edca401b339d939a6598d0f1682882b0ed7b2fa3
SHADOW-1104 <ing.colin@outlook.com>
2024-08-07 21:18:15 -0600

    trycatch added

--- RandomJokes/js/script.js
+++ RandomJokes/js/script.js
@@ -16,17 +16,69 @@ const icons = {
 
 const iconsCounter = Object.keys(icons)
 
+// Peticion con FETCH sin TRY-CATCH
 const getJoke = () => {
     jokeContainer.classList.remove('fade')
     fetch(API_URL)
-    .then(data => data.json())
-    .then(item => {
-        jokeContainer.textContent = `${item.joke}`
-        jokeContainer.classList.add('fade')
-        const randomIcon = icons[Math.floor(Math.random() * iconsCounter.length)]
-        icon.textContent = randomIcon
-    })
+        .then(data => data.json())
+        .then(item => {
+            jokeContainer.textContent = `${item.joke}`
+            jokeContainer.classList.add('fade')
+            const randomIcon = icons[Math.floor(Math.random() * iconsCounter.length)]
+            icon.textContent = randomIcon
+        })
 }
+
+
+// Peticion con FETCH + TRY-CATCH
+// const getJoke = () => {
+//     jokeContainer.classList.remove('fade');
+
+//     fetch(API_URL)
+//         .then(response => {
+//             if (!response.ok) {
+//                 throw new Error('Problema con la API');
+//             }
+//             return response.json();
+//         })
+//         .then(item => {
+//             jokeContainer.textContent = `${item.joke}`;
+//             jokeContainer.classList.add('fade');
+
+//             const randomIcon = icons[Math.floor(Math.random() * icons.length)];
+//             icon.textContent = randomIcon;
+//         })
+//         .catch(error => {
+//             console.error('Error:', error);
+//             alert('No pudimos procesar tu petición');
+//         });
+// };
+
+
+// Peticion con ASYNC AWAIT + TRY-CATCH
+// const fetchJokes = async () => {
+//     try{
+//         jokeContainer.classList.remove('fade')
+//         const response = await fetch(API_URL)
+//         if(!response.ok){
+//             throw new Error("Problema con la API", error);
+//         }
+//         const data = await response.json()
+//         jokeContainer.textContent = data.joke;
+//         jokeContainer.classList.add('fade')
+//         const randomIcon = icons[Math.floor(Math.random() * iconsCounter.length)]
+//         icon.textContent = randomIcon
+//         console.log(data.joke)
+
+//     }catch (error) {
+//         alert('No pudimos procesar tu peticion' , error)
+//     }
+
+// }
+// btn.addEventListener('click', fetchJokes)
+// fetchJokes()
+
+
 
 btn.addEventListener('click', getJoke)
 getJoke()
